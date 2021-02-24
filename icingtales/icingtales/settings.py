fead52f431e73a754f8fda5dafb4a52aaa85dfca
dahalbhawan <dahalbhawan@gmail.com>
2021-02-24 12:25:24 +1100

    Update settings.py

--- icingtales/icingtales/settings.py
+++ icingtales/icingtales/settings.py
@@ -154,7 +154,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dahalbhawan@gmail.com'
-EMAIL_HOST_PASSWORD = '********'
+EMAIL_HOST_PASSWORD = '9808148986maniDahal'
 
 PAYPAL_RECEIVER_EMAIL = 'dahalbhawan@gmail.com'
 PAYPAL_TEST = True
